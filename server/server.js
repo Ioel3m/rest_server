ac010cb35ea2d7b3aee37c1fedc61443460aaa4d
ioel3m <jl3moran@gmail.com>
2018-10-26 17:02:05 -0500

    SERVER.JS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(require('./routes/usuario'))
 
 
 
-mongoose.connect(process.env.URLDB, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
     if (err) throw err
     console.log("Base de datos online...")
 });
